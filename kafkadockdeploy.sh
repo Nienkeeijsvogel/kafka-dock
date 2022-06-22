c6b65c2d4f9465064915b735ee3a4d4baabd7470
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-06-22 11:21:51 +0200

    Update kafkadockdeploy.sh

--- kafkadockdeploy.sh
+++ kafkadockdeploy.sh
@@ -7,7 +7,7 @@ yum install -y yum-utils
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 systemctl start docker
 
 setenforce 0
